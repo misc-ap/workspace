9601741ec0b401b932339d7f3498bebb027d8645
misc-ap <accmisc751@gmail.com>
2025-04-26 18:19:14 +0530

    formatted code

--- fileOpener.c
+++ fileOpener.c
@@ -1,26 +1,31 @@
-#include<stdio.h>
-int main(int argc,char *argv[])
+#include <stdio.h>
+int main(int argc, char *argv[])
 {
-  if(argc<2){
+  if (argc < 2)
+  {
     printf("No argument provided!\n");
     return 1;
   }
   FILE *fp;
   char ch;
-  int i=1;
-  while(i<argc){
-    fp=fopen(argv[i],"r");
-    if (fp == NULL) {
+  int i = 1;
+  while (i < argc)
+  {
+    fp = fopen(argv[i], "r");
+    if (fp == NULL)
+    {
       perror(argv[i]);
-      fprintf(stderr,"(make sure the name is correct with extension or you are in correct directory)\n");
+      fprintf(stderr, "(make sure the name is correct with extension or you are in correct directory)\n");
       i++;
       continue;
     }
-    if(argc>2) {
+    if (argc > 2)
+    {
       printf("\n----- Reading file: %s -----\n", argv[i]);
     }
-    while ((ch = fgetc(fp)) != EOF) {
-      printf("%c",ch);
+    while ((ch = fgetc(fp)) != EOF)
+    {
+      printf("%c", ch);
     }
     fclose(fp);
     i++;
--- gridGame.c
+++ gridGame.c
@@ -1,73 +1,91 @@
-#include<stdio.h>
-void clear(){
-   printf("\033[2J\033[H");
+#include <stdio.h>
+void clear()
+{
+  printf("\033[2J\033[H");
 }
 
-void swap(int *a,int *b){
-  int temp=*a;
-  *a=*b;
-  *b=temp;
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 int main()
 {
-  
-  int n,q=0;
+
+  int n, q = 0;
   char b;
   printf("Enter size : ");
-  scanf("%d",&n);
-  int xy[n][n],x=n/2,y=n/2;
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-      xy[i][j]=0;
+  scanf("%d", &n);
+  int xy[n][n], x = n / 2, y = n / 2;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      xy[i][j] = 0;
     }
   }
-  xy[x][y]=1;
-  while(1){
+  xy[x][y] = 1;
+  while (1)
+  {
     clear();
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
       printf(" ");
-        for (int j = 0; j < n; j++) {
-          if (xy[i][j] == 1) {
-            printf("\033[38;2;255;255;255m\033[1m0\033[0m ");
-          }else{
-            printf("\033[38;2;100;100;100m.\033[0m ");
-          }
+      for (int j = 0; j < n; j++)
+      {
+        if (xy[i][j] == 1)
+        {
+          printf("\033[38;2;255;255;255m\033[1m0\033[0m ");
+        }
+        else
+        {
+          printf("\033[38;2;100;100;100m.\033[0m ");
+        }
       }
       printf("\n");
     }
     printf("\033[0m");
     printf("Use /W/A/S/D to move, any other key to quit: ");
-    scanf(" %c",&b);
-    switch(b){
-     case 'w':
-        if (x > 0) {
-          swap(&xy[x][y], &xy[x - 1][y]);
-          x--;
-        }
-        break;
-      case 's':
-        if (x < n - 1) {
-          swap(&xy[x][y], &xy[x + 1][y]);
-          x++;
-        }
-        break;
-      case 'a':
-        if (y > 0) {
-          swap(&xy[x][y], &xy[x][y - 1]);
-          y--;
-        }
-        break;
-      case 'd':
-        if (y < n - 1) {
-          swap(&xy[x][y], &xy[x][y + 1]);
-          y++;
-        }
-        break;
-     default:
-        q=1;
+    scanf(" %c", &b);
+    switch (b)
+    {
+    case 'w':
+      if (x > 0)
+      {
+        swap(&xy[x][y], &xy[x - 1][y]);
+        x--;
+      }
+      break;
+    case 's':
+      if (x < n - 1)
+      {
+        swap(&xy[x][y], &xy[x + 1][y]);
+        x++;
+      }
+      break;
+    case 'a':
+      if (y > 0)
+      {
+        swap(&xy[x][y], &xy[x][y - 1]);
+        y--;
+      }
+      break;
+    case 'd':
+      if (y < n - 1)
+      {
+        swap(&xy[x][y], &xy[x][y + 1]);
+        y++;
+      }
+      break;
+    default:
+      q = 1;
     }
-    if(q){break;}
+    if (q)
+    {
+      break;
+    }
   }
   clear();
   return 0;
