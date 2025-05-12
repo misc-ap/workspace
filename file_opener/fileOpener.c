6217d759835d1dc28a82e32aa8319c4affdb408c
misc-ap <accmisc751@gmail.com>
2025-05-12 21:38:23 +0530

    updated fileopener.c

--- file_opener/fileOpener.c
+++ file_opener/fileOpener.c
@@ -3,21 +3,21 @@ int main(int argc, char *argv[])
 {
   if (argc < 2)
   {
-    printf("No argument provided!(use -h for help)\n");
+    printf("\x1b[31mNo argument provided!(use -h for help)\x1b[0m\n");
     return 1;
   }
   int j = 1;
   while (argc > j && argv[j][0] == '-')
   {
-    if (argv[1][1] == 'h')
+    if (argv[1][1] == 'h' && argv[1][2] == '\0')
     {
-      printf("+ Usage: fdisp <file1> <file2> ...\n+ fdisp -h for help\n");
+      printf("\x1b[33m+ Usage: fdisp <file1> <file2> ...\n+ fdisp -h for help\x1b[0m\n");
       printf("This program reads the contents of the specified files and prints them to the standard output.\n");
       return 0;
     }
     else
     {
-      printf("Invalid option: %s\n", argv[1]);
+      printf("\x1b[31mInvalid option: %s\x1b[0m\n", argv[1]);
       return 1;
     }
     j++;
@@ -29,9 +29,9 @@ int main(int argc, char *argv[])
   {
     fp = fopen(argv[i], "r");
     if (fp == NULL)
-    {
+    { 
       perror(argv[i]);
-      fprintf(stderr, "(make sure the name is correct with extension or you are in correct directory)\n");
+      printf("\x1b[33m(make sure the name is correct with extension or you are in correct directory)\x1b[0m\n");
       i++;
       continue;
     }
